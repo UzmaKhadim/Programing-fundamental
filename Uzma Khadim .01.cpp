2df4f86e2fe92afe1b28cfe8ca76fe21a27eef41
UzmaKhadim <uzmakhadim166@gmail.com>
2025-11-26 00:00:08 -0800

    Add files via upload

---
+++ Uzma Khadim .01.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"\n\t\t------------------------------------";
+	cout<<"\n\t\t STUDENT ID CARD\t\t";
+	cout<<"\n\t\t-----------------------------------";
+	cout<<"\n\t\tName:\t\tUzma Khadim\t\t";
+	cout<<"\n\t\tDepartment:Software Engineering\t\t";
+	cout<<"\n\t\tSession:\t2025-2029\t\t";
+	cout<<"\n\t\t-----------------------------------";
+	return 0;
+}
