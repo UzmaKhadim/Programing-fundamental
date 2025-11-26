1e5566aefac1cd5ebeed10a15ad9fa9a0590c862
UzmaKhadim <uzmakhadim166@gmail.com>
2025-11-26 10:09:23 -0800

    Add files via upload

---
+++ uzma khadim task 4.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"\n+--------------------------------------------";
+	cout<<"\n|\tRoll No\t|\tMarks\t|";
+	cout<<"\n|\t101\t|\t89\t|";
+	cout<<"\n|\t109\t|\t79\t|";
+	cout<<"\n|\t115\t|\t95\t|";
+	cout<<"\n+--------------------------------------------";
+	return 0;
+	
+}
