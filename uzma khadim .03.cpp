dd6b79ebeebd88661b30b350a62ff9c36e9b9424
UzmaKhadim <uzmakhadim166@gmail.com>
2025-11-26 00:26:48 -0800

    Add files via upload

---
+++ uzma khadim .03.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+cout<<"\n--------------------------------------------------";
+cout<<"\n\tCertificates of achivements";
+cout<<"\n--------------------------------------------------";
+cout<<"\nThis is a certify that'Ali Raza'\n Has successfully completed the course\nC++ Programing with A+ Grade.";
+cout<<"\n-------------------------------------------------";
+return 0;
+}
