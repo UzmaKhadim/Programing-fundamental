314ab29e62dbc77f2c0c7f7d893f03afb8841078
UzmaKhadim <uzmakhadim166@gmail.com>
2025-11-26 00:12:28 -0800

    Add files via upload

---
+++ uzma khadim .02.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"\n******************************MENU***************************";
+	cout<<"\n1.\tBiryani\t\tRs.\t250";
+	cout<<"\n2.\tKarachi\t\tRs.\t1200";
+	cout<<"\n3.\tQorma\t\tRs.\t650";
+	cout<<"\n4.\tDrinks\t\t.Rs.\t550";
+	cout<<"**************************************************************";
+	return 0;
+}
