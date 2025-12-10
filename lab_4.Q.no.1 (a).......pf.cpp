9b2e7d68c42b429ab19938c2873694e4c3ff4f71
UzmaKhadim <uzmakhadim166@gmail.com>
2025-12-10 11:41:08 -0800

    Add files via upload

---
+++ lab_4.Q.no.1 (a).......pf.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+ {
+    int burgerPrice = 350;
+    int drinkPrice = 120;
+    int friesPrice = 150;
+    int burgers = 2;
+    int drinks = 1;
+    int fries = 1;
+    int totalBill = (burgers * burgerPrice) + (drinks * drinkPrice) + (fries * friesPrice);
+    float tax = totalBill * 0.07;
+    float finalAmount = totalBill + tax;
+    int totalItems = burgers + drinks + fries;
+    float averageCost = finalAmount / totalItems;
+    cout << "Total Bill: " << totalBill << " PKR" << endl;
+    cout << "Sales Tax (7%): " << tax << " PKR" << endl;
+    cout << "Final Amount: " << finalAmount << " PKR" << endl;
+    cout << "Average Cost per Item: " << averageCost << " PKR" << endl;
+    return 0;
+}
+
+
