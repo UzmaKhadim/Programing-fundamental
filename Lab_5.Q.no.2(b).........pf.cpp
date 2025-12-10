cb1d5020a14972759e3e48c4a124cba9020074d0
UzmaKhadim <uzmakhadim166@gmail.com>
2025-12-10 11:13:38 -0800

    Add files via upload

---
+++ Lab_5.Q.no.2(b).........pf.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+ {
+    int marks;
+    cout << "Enter original assignment marks: ";
+    cin >> marks;
+    marks += 5;
+    cout << "Total marks after grace: " << marks;
+    return 0;
+}
+
+
