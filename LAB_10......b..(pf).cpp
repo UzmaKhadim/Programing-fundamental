bcd2878e656b09db47cb581bced99ac6e81451b9
UzmaKhadim <uzmakhadim166@gmail.com>
2026-01-06 10:04:38 -0800

    Add files via upload

---
+++ LAB_10......b..(pf).cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 1;       
+    int sum = 0;     
+
+    while (i <= 10) {
+        sum = sum + i;  
+        i++;            
+    }
+
+    cout << "Sum of first 10 natural numbers = " << sum << endl;
+
+    return 0;
+}
